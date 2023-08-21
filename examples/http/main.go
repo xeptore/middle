4ebc5fc03c70fac7c6f78663e247b405082f4d20
xeptore <29199390+xeptore@users.noreply.github.com>
2023-08-21 15:10:03 +0330

    add examples
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

---
+++ examples/http/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/xeptore/middle"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/path", middle.Ware3(m1, m2, m3).Then(handler))
+	http.ListenAndServe("127.0.0.1:1080", mux)
+}
+
+func m1(http.ResponseWriter, *http.Request) (string, error) {
+	fmt.Println("in m1!")
+	return "First!", nil
+}
+
+func m2(http.ResponseWriter, *http.Request) (int, error) {
+	fmt.Println("in m2!")
+	return -42, nil
+}
+
+func m3(http.ResponseWriter, *http.Request) (bool, error) {
+	fmt.Println("in m3!")
+	return true, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request, s string, i int, b bool) {
+	w.Write([]byte(fmt.Sprintf("%s\n%d\n%v", s, i, b)))
+}
